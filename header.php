816ed41f879fe6aea62dc3920cf7814b8384bb1d
Germano Corrêa <germanocorrea@coffeebased.esy.es>
2015-01-10 11:33:26 -0200

    Adicionado formulário de pesquisa à barra superior e implementos no menu slidepush, como mensagem de boas vindas (não formatada) para o usuário e botão de log out/log in.

--- header.php
+++ header.php
@@ -14,30 +14,34 @@
             <div class="colored-bar"></div>
 			<div class="bar">
                 <button id="showLeftPush" class="menu_button"><i class="fa fa-navicon fa-2x"></i></button>
-                <div class="sign-in">
-                        <?php
-                            $user_id = get_current_user_id();
-                            $current_user = wp_get_current_user();
-    	                    $profile_url = get_edit_profile_url( $user_id );
-                            if ( is_user_logged_in() ) {
-                                echo 'Olá ',the_author_meta('display_name', $user_id);
-                            }
-                            else {
-                                echo 'Login';
-                            }
-                        ?>
+                <div class="search">
+                    <?php
+                        $search_form=get_search_form();
+                        echo $search_form;
+                    ?>
                 </div>
             </div>
 		</header>
 		<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
-
-			<a href="#"><i class="fa fa-home fa-1x"></i>&nbsp;&nbsp;Página Inicial</a>
+            <div class="user">
+                <?php
+                $user_id = get_current_user_id();
+                $current_user = wp_get_current_user();
+                $profile_url = get_edit_profile_url( $user_id );
+                if ( is_user_logged_in() ) {
+                    echo 'Olá ',the_author_meta('display_name', $user_id);
+                }
+                else {
+                    echo 'Login';
+                }
+                ?>
+            </div>
+			<a href="<?php echo get_site_url(); ?>"><i class="fa fa-home fa-1x"></i>&nbsp;&nbsp;Página Inicial</a>
 			<a href="#"><i class="fa fa-newspaper-o fa-1x"></i>&nbsp;&nbsp;Notícias</a>
 			<a href="#"><i class="fa fa-globe fa-1x"></i>&nbsp;&nbsp;O Universo</a>
 			<a href="#"><i class="fa fa-users fa-1x"></i>&nbsp;&nbsp;Comunidade</a>
 			<a href="#"><i class="fa fa-code fa-1x"></i>&nbsp;&nbsp;O Site</a>
-			<hr>
-			<a href="#"><i class="fa fa-user fa-1x"></i>&nbsp;&nbsp;Sign In (em breve)</a>
+			<hr><?php wp_loginout(); ?>
 			<!--<a href="#"><i class="fa fa-arrow-left fa-1x"></i>&nbsp;&nbsp;Esconder Menu</a>-->
 		</nav>
 		<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/classie.js"></script>
