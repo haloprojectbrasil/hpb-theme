1ac29314567b50e8b1631a0858e5a8b5d2e59c1b
Germano Corrêa <germanocorrea@coffeebased.esy.es>
2015-01-14 14:48:03 -0200

    Trocada ordem dos objetos do rodapé.

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
     <?php wp_footer(); ?>
     <div class="footer">
         <div id="footer-area">
+            <h1>Licença e Termos de Uso</h1>
+            <p>Halo © Microsoft Corporation. Halo Project Brasil was created under Microsoft's <a href="http://www.xbox.com/en-US/community/developer/rules">"Game Content Usage   Rules"</a> using assets from Halo. It is  not  endorsed by Microsoft and does not reflect the views or opinions of  Microsoft  or anyone officially involved in producing or managing Halo.  As such, it does not contribute to the  official narrative of the fictional  universe, if applicable.</p>
+            <p><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Halo Project Brasil</span> de
+                <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.haloprojectbrasil.com.br" property="cc:attributionName" rel="cc:attributionURL">http://www.haloprojectbrasil.com.br</a> está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons - Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional</a>.</p>
+            <p> O sistema de Halo Project Brasil de <a href="http://www.haloprojectbrasil.com.br">http://www.haloprojectbrasil.com.br</a> possui uma licença Open Source MIT e seu código fonte se encontra no GitHub.</p>
+        </div>
+        <div id="footer-area">
             <h1>Parceiros</h1>
             <a href="https://www.youtube.com/user/sidaosidney" target=_blank><img src="http://1.bp.blogspot.com/-r7sz1IKoZ6I/U6uh9COKrKI/AAAAAAAAAsY/kPgZssHJr0w/s1600/photo.jpg" width=150 float=left /></a>
 <br /><br/>
@@ -14,13 +21,6 @@
             <img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/instagram.png"/><a id="link" href="http://instagram.com/haloprojectbr">&nbsp;&nbsp;&nbsp;@haloprojectbr<a/><br/><br/>
             <img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/youtube.png"/><a id="link" href="https://www.youtube.com/user/haloprojectbr">&nbsp;&nbsp;&nbsp;Halo Project Brasil<a/><br/><br/>
             <img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/github.png"/><a id="link" href="https://github.com/haloprojectbrasil/">&nbsp;&nbsp;&nbsp;Halo Project Brasil<a/><br/><br/>
-        </div>
-        <div id="footer-area">
-            <h1>Licença e Termos de Uso</h1>
-            <p>Halo © Microsoft Corporation. Halo Project Brasil was created under Microsoft's <a href="http://www.xbox.com/en-US/community/developer/rules">"Game Content Usage   Rules"</a> using assets from Halo. It is  not  endorsed by Microsoft and does not reflect the views or opinions of  Microsoft  or anyone officially involved in producing or managing Halo.  As such, it does not contribute to the  official narrative of the fictional  universe, if applicable.</p>
-            <p><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Halo Project Brasil</span> de
-                <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.haloprojectbrasil.com.br" property="cc:attributionName" rel="cc:attributionURL">http://www.haloprojectbrasil.com.br</a> está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons - Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional</a>.</p>
-            <p> O sistema de Halo Project Brasil de <a href="http://www.haloprojectbrasil.com.br">http://www.haloprojectbrasil.com.br</a> possui uma licença Open Source MIT e seu código fonte se encontra no GitHub.</p>
         </div>
     </div>
     </body>
