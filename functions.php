34dc4360b9ea9ab557f2be0c51e449a9ceaec3f1
Germano Corrêa <germanocorrea@coffeebased.esy.es>
2015-02-17 22:52:46 -0200

    Consertado a falta de um <title> da front-page.php pelo arquivo de funções.

--- functions.php
+++ functions.php
@@ -4,4 +4,12 @@ remove_action ('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_generator');
 add_theme_support( 'post-thumbnails' );
 ini_set( 'mysql.trace_mode', 0 );
+add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
+function baw_hack_wp_title_for_home( $title )
+{
+    if( empty( $title ) && ( is_home() || is_front_page() ) ) {
+        return __( 'Halo Project Brasil', 'theme_domain' ) . ' | ' . get_bloginfo( 'description' );
+    }
+    return $title;
+}
 ?>
