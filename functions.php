f8577e920e113efd0bc5bc69d7cfd3daa72c47b6
Germano Corrêa <germanocorrea@coffeebased.esy.es>
2015-02-17 22:39:17 -0200

    Criado favicons: Desktop, iOS, Android e Windows 8.

--- functions.php
+++ functions.php
@@ -1,9 +1,7 @@
 <?php
 add_filter('show_admin_bar', '__return_false');
+remove_action ('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_generator');
 add_theme_support( 'post-thumbnails' );
-add_image_size( 'small', 430, 286, true );
-add_image_size( 'large', 1140, 1140, false );
-add_image_size( 'thumbnails', 200, 133, true );
 ini_set( 'mysql.trace_mode', 0 );
 ?>
--- header.php
+++ header.php
@@ -7,6 +7,23 @@
             <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
             <title><?php wp_title(''); ?></title>
+            <link rel="apple-touch-icon" sizes="57x57" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-57x57.png">
+            <link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-60x60.png">
+            <link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-72x72.png">
+            <link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-76x76.png">
+            <link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-114x114.png">
+            <link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-120x120.png">
+            <link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-144x144.png">
+            <link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-152x152.png">
+            <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/apple-touch-icon-180x180.png">
+            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/favicon-32x32.png" sizes="32x32">
+            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/android-chrome-192x192.png" sizes="192x192">
+            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/favicon-96x96.png" sizes="96x96">
+            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/favicon-16x16.png" sizes="16x16">
+            <link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() ); ?>/fav/manifest.json">
+            <meta name="msapplication-TileColor" content="#010716">
+            <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+            <meta name="theme-color" content="#010716">
             <?php wp_head(); ?>
         </head>
     <body class="cbp-spmenu-push" <?php body_class(); ?>>
