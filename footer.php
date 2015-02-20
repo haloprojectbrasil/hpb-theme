ae93902e8d63dd34aab11db6beb3964f7ff2ea45
Germano Corrêa <germanocorrea@coffeebased.esy.es>
2015-02-20 11:10:02 -0200

    Update da licença no rodapé, e adição no logo do cabeçalho

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="footer">
         <div id="footer-area">
             <h1>Licença e Termos de Uso</h1>
-            <p>Halo © Microsoft Corporation. Halo Project Brasil was created under Microsoft's <a href="http://www.xbox.com/en-US/community/developer/rules">"Game Content Usage   Rules"</a> using assets from Halo. It is  not  endorsed by Microsoft and does not reflect the views or opinions of  Microsoft  or anyone officially involved in producing or managing Halo.  As such, it does not contribute to the  official narrative of the fictional  universe, if applicable.</p>
+            <p>Halo © Microsoft Corporation. Halo Project Brasil was created under Microsoft's <a href="http://www.xbox.com/en-US/developers/rules">"Game Content Usage   Rules"</a> using assets from Halo, and it is not endorsed by or affiliated with Microsoft.</p>
             <p><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Halo Project Brasil</span> de
                 <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.haloprojectbrasil.com.br" property="cc:attributionName" rel="cc:attributionURL">http://www.haloprojectbrasil.com.br</a> está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons - Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional</a>.</p>
             <p> Os temas de Halo Project Brasil de <a href="http://www.haloprojectbrasil.com.br">http://www.haloprojectbrasil.com.br</a> possuem uma licença GPL e seu código fonte se encontra no GitHub.</p><br/><br/><br/>
