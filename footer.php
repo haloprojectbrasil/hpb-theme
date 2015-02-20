b85e7883830094e8be8bf17b7704e946e9e47fab
Germano Corrêa <germanocorrea@coffeebased.esy.es>
2015-02-20 10:55:47 -0200

    Redes sociais removidas do rodapé e adicionadas na sidebar, adicionado google plus a elas

--- footer.php
+++ footer.php
@@ -5,12 +5,7 @@
             <p>Halo © Microsoft Corporation. Halo Project Brasil was created under Microsoft's <a href="http://www.xbox.com/en-US/community/developer/rules">"Game Content Usage   Rules"</a> using assets from Halo. It is  not  endorsed by Microsoft and does not reflect the views or opinions of  Microsoft  or anyone officially involved in producing or managing Halo.  As such, it does not contribute to the  official narrative of the fictional  universe, if applicable.</p>
             <p><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Halo Project Brasil</span> de
                 <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.haloprojectbrasil.com.br" property="cc:attributionName" rel="cc:attributionURL">http://www.haloprojectbrasil.com.br</a> está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons - Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional</a>.</p>
-            <p> O sistema de Halo Project Brasil de <a href="http://www.haloprojectbrasil.com.br">http://www.haloprojectbrasil.com.br</a> possui uma licença Open Source MIT e seu código fonte se encontra no GitHub.</p>
-            <img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/facebook.png"/>&nbsp;&nbsp;&nbsp;<a id="link" href="https://www.facebook.com/TheHaloProjectBR"><a/>
-            <img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/twitter.png"/>&nbsp;&nbsp;&nbsp;<a id="link" href="https://twitter.com/haloprojectbr"><a/>
-            <img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/instagram.png"/>&nbsp;&nbsp;&nbsp;<a id="link" href="http://instagram.com/haloprojectbr"><a/>
-            <img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/youtube.png"/>&nbsp;&nbsp;&nbsp;<a id="link" href="https://www.youtube.com/user/haloprojectbr"><a/>
-            <img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/github.png"/>&nbsp;&nbsp;&nbsp;<a id="link" href="https://github.com/haloprojectbrasil/"><a/><br/>
+            <p> Os temas de Halo Project Brasil de <a href="http://www.haloprojectbrasil.com.br">http://www.haloprojectbrasil.com.br</a> possuem uma licença GPL e seu código fonte se encontra no GitHub.</p><br/><br/><br/>
         </div>
     </div>
     </body>
--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,20 @@
 <div class="side">
 	<div class="sidebar">
-		<h1>Youtube</h1>
-		<iframe width="100%" src="https://www.youtube.com/embed/S9PsA2b5fF0" frameborder="0" allowfullscreen></iframe>
+		<div id="youtubeSide">
+			<h1>Youtube</h1>
+			<iframe width="100%" height="75%" src="https://www.youtube.com/embed/S9PsA2b5fF0" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>
+	<div class="sidebar">
+		<h1>Redes Sociais</h1>
+		<center>
+			<a id="link" href="https://www.facebook.com/TheHaloProjectBR"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/facebook.png"/><a/>
+				<a id="link" href="https://twitter.com/haloprojectbr"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/twitter.png"/><a/>
+					<a id="link" href="http://instagram.com/haloprojectbr"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/instagram.png"/><a/>
+						<a id="link" href="https://www.youtube.com/user/haloprojectbr"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/youtube.png"/><a/>
+							<a id="link" href="https://plus.google.com/108741675368093970567/"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/googleplus.png"/><a/>
+							<a id="link" href="https://github.com/haloprojectbrasil/"><img id="social" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/github.png"/><a/>
+		</center>
 	</div>
 	<div class="sidebar">
 		<h1>Parceiros</h1>
@@ -9,9 +22,14 @@
 		<br />
 		<a href="https://www.youtube.com/user/sidaosidney" target=_blank><img src="http://1.bp.blogspot.com/-r7sz1IKoZ6I/U6uh9COKrKI/AAAAAAAAAsY/kPgZssHJr0w/s1600/photo.jpg" width="100%" /></a>
 		<br />
+		<a href="https://www.facebook.com/pages/Protocolo-Halo/763261090381596" target=_blank><img src="http://i.imgur.com/Muaqimv.png" width="100%" /></a>
+		<br />
 		<a href="http://www.facebook.com/groups/281755052018802/?fref=ts" target=_blank><img src="http://i.imgur.com/Efyrjyn.png?1" width="100%" /></a>
 		<br />
 		<a href="https://www.facebook.com/pages/Halo-Brasil/128394730646803" target=_blank><img src="http://i.imgur.com/0PR896V.png?1" width="100%" /></a>
+		<a href="https://www.facebook.com/pages/Na%C3%A7%C3%A3o-Halo/160833494109647?fref=ts" target=_blank><img src="http://i.imgur.com/6jScEZA.png" width="100%" /></a>
 		<br />
+		<a href="https://www.youtube.com/channel/UChq8cm_ms8tnHAiYHIAKJ8A" target=_blank><img src="http://i.imgur.com/zJQn8ft.png" width="100%" /></a>
+			<br />
 	</div>
 </div>
